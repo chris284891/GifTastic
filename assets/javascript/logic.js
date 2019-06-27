82572172a7038937b733fb2474da8e63b8e52f41
chris284891 <chris284891@gmail.com>
2019-06-27 14:40:04 -0400

    added on click listener, for loop, and if statement

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,28 @@
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=JnGmCnV1TZYxyeo7aniDfwJIqUqB8wDX";
+
+
+$("button").on("click", function () {
+    var bands = $(this).attr("data-bands");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + bands + "&api_key=JnGmCnV1TZYxyeo7aniDfwJIqUqB8wDX";
 
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
     });
+    var topic = response.data;
+
+    for (var i = 0; i < topic.length; i++) {
+        if (topic[i].rating !== "r" && topic[i].rating !== "pg-13") {
+            var gifSpace = $("<div>");
+            var rating = topic[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var bandImage = $("<img>");
+            bandImage.attr("src", topic[i].images.fixed_height.url);
+            gifSpace.append(p);
+            gifSpace.append(bandImage);
+            $("#bandGifs").prepend(gifSpace);
+        }
+    }
+});
